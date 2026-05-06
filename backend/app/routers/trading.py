e7e77255733849937949724f5d8ee02f0dd1c3ac
vitalinka22 <vitalinka.alipova2002@gmail.com>
2026-05-06 10:03:35 +0200

    connect trading endpoints to database1

--- backend/app/routers/trading.py
+++ backend/app/routers/trading.py
@@ -1,5 +1,16 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 from pydantic import BaseModel 
+from sqlalchemy.orm import Session
+from app.db.database import SessionLocal
+from app.models.trade import Trade
+from app.models.users import User
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 router = APIRouter()
 
@@ -19,29 +30,79 @@ def get_current_price(ticker: str) -> float:
 class TradeRequest(BaseModel):
     ticker: str
     quantity: int
+    user_id: int # temporary - later comes from JWT token
 
 @router.post("/trades/buy")
-def buy_stock(trade: TradeRequest):
+def buy_stock(trade: TradeRequest, db : Session = Depends(get_db)):
+
+    #find the user in database
+    user = db.query(User).filter(User.id == trade.user_id).first()
+    if not user:
+        raise HTTPException(status_code = 404, detail = 'User not found')
+    
     price = get_current_price(trade.ticker)
     total_cost = price*trade.quantity
 
+    if user.balance < total_cost:
+        raise HTTPException(
+            status_code = 400,
+            detail = f"Insufficient funds. Need ${total_cost}, have ${user.balance}"
+        )
+    
+    
+    user.balance -= total_cost
+
+    new_trade = Trade(
+    user_id=user.id,
+    ticker=trade.ticker,
+    trade_type="buy",
+    quantity=trade.quantity,
+    price=price,
+    total_value=total_cost,
+    )
+    db.add(new_trade)
+    db.commit()
+
     return {
-        "message": "Buy order received",
+        "message": "Buy order successful",
         "ticker": trade.ticker,
         "quantity":trade.quantity, 
         "price_per_stock": price, 
-        "total_cost" : total_cost
+        "total_cost" : total_cost,
+        "new_balance": user.balance,
     }   
 
 @router.post("/trades/sell")
-def sell_stock(trade: TradeRequest):
+def sell_stock(trade: TradeRequest, db: Session = Depends(get_db)):
+
+    user = db.query(User).filter(User.id == trade.user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
     price = get_current_price(trade.ticker)
     total_value = price * trade.quantity
 
+    new_trade = Trade(
+        user_id=user.id,
+        ticker=trade.ticker,
+        trade_type="sell",
+        quantity=trade.quantity,
+        price=price,
+        total_value=total_value,
+    )
+    db.add(new_trade)
+
+    user.balance += total_value
+    db.commit()
+
     return {
-        "message": "Sell order received",
+        "message": "Sell order successful",
         "ticker": trade.ticker,
         "quantity": trade.quantity,
         "price_per_stock": price,
         "total_value": total_value,
+        "new_balance": user.balance,
     }
+
+
+
